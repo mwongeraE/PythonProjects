ec7832d39b3cfb545d3e3f5556644bf8d3361355
mwongeraE <evansmwongera@gmail.com>
2017-10-12 00:16:41 +0300

    Control + looping

--- CodeAcademySolutions/Control.py
+++ CodeAcademySolutions/Control.py
@@ -14,3 +14,10 @@ webster = {
 # Add your code below!
 for key in webster:
   print webster[key]
+
+#### Control flow and looping
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for x in a:
+  if x % 2 == 0:
+    print x
