63fad281a19e25479824fd16b4fba41431c7bc57
mwongeraE <evansmwongera@gmail.com>
2017-10-11 21:16:47 +0300

    Cube function

--- CodeAcademySolutions/Functions.py
+++ CodeAcademySolutions/Functions.py
@@ -29,3 +29,13 @@ def one_good_turn(n):
 
 def deserves_another(n):
   return one_good_turn(n) + 2
+
+def cube(number):
+  return number ** 3
+
+#Cube function
+def by_three(number):
+  if number % 3 == 0:
+  	return cube(number)
+  else:
+    return False
