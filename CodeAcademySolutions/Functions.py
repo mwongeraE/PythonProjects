18d4fe556142cf29da0dd6d1d03e6c94f069ba2b
mwongeraE <evansmwongera@gmail.com>
2017-10-11 21:25:53 +0300

    Dragons

--- CodeAcademySolutions/Functions.py
+++ CodeAcademySolutions/Functions.py
@@ -44,3 +44,20 @@ def by_three(number):
 import math # Imports the math module
 everything = dir(math) # Sets everything to a list of things from math
 print everything # Prints 'em all!
+
+#Here be dragons
+def biggest_number(*args):
+  print max(args)
+  return max(args)
+
+def smallest_number(*args):
+  print min(args)
+  return min(args)
+
+def distance_from_zero(arg):
+  print abs(arg)
+  return abs(arg)
+
+biggest_number(-10, -5, 5, 10)
+smallest_number(-10, -5, 5, 10)
+distance_from_zero(-10)
