16792485f4b289a33008aefa52ea7c69d080d56a
mwongeraE <evansmwongera@gmail.com>
2017-10-11 20:59:11 +0300

    square function

--- CodeAcademySolutions/Functions.py
+++ CodeAcademySolutions/Functions.py
@@ -4,3 +4,13 @@ def spam():
 
 # Define the spam function above this line.
 spam()
+
+def square(n):
+  """Returns the square of a number."""
+  squared = n ** 2
+  print "%d squared is %d." % (n, squared)
+  return squared
+
+# Call the square function on line 10! Make sure to
+# include the number 10 between the parentheses.
+square(10)
