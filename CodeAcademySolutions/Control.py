01738808c540716714a8ea782a2055894f441d60
mwongeraE <evansmwongera@gmail.com>
2017-10-12 00:13:52 +0300

    for in list and dics

---
+++ CodeAcademySolutions/Control.py
@@ -0,0 +1,16 @@
+#### For in Lists
+names = ["Adam","Alex","Mariah","Martine","Columbus"]
+for x in names:
+  print x
+
+####for in Dictionaries
+webster = {
+  "Aardvark" : "A star of a popular children's cartoon show.",
+  "Baa" : "The sound a goat makes.",
+  "Carpet": "Goes on the floor.",
+  "Dab": "A small amount."
+}
+
+# Add your code below!
+for key in webster:
+  print webster[key]
