6f748504e07668932e3d6d46f72def5a523a09ab
mwongeraE <evansmwongera@gmail.com>
2017-10-11 23:07:52 +0300

    Index

--- CodeAcademySolutions/List_and_Dictionaries.py
+++ CodeAcademySolutions/List_and_Dictionaries.py
@@ -6,3 +6,13 @@ if len(zoo_animals) > 3:
   print "The second animal at the zoo is the " + zoo_animals[1]
   print "The third animal at the zoo is the " + zoo_animals[2]
   print "The fourth animal at the zoo is the " + zoo_animals[3]
+
+#Index
+zoo_animals = ["pangolin", "cassowary", "sloth", "tiger"]
+
+# The ferocious sloth has been replaced by a friendly hyena.
+zoo_animals[2] = "hyena"
+
+# What shall fill the void left by our dear departed tiger?
+# Your code here!
+zoo_animals[3] = "cow"
