b828bd10434ed10b1860b45023341f7cb004d48e
mwongeraE <evansmwongera@gmail.com>
2017-10-11 23:19:50 +0300

    Maintaining order

--- CodeAcademySolutions/List_and_Dictionaries.py
+++ CodeAcademySolutions/List_and_Dictionaries.py
@@ -42,3 +42,12 @@ middle = suitcase[2:4]
 
 # The last two items (index four and five)
 last =  suitcase[4:6]
+
+#Maintaining order
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+duck_index = animals.index("duck")# Use index() to find "duck"
+
+# Your code here!
+animals.insert(duck_index, "cobra")
+
+print animals # Observe what prints after the insert operation
