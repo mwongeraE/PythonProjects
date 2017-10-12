b940008b9e761a4b6709883409d8114d2167479c
mwongeraE <evansmwongera@gmail.com>
2017-10-12 20:30:39 +0300

    Simple Errors

--- CodeAcademySolutions/Loops.py
+++ CodeAcademySolutions/Loops.py
@@ -5,3 +5,10 @@ while num <= 10:  # Fill in the condition
   print num ** 2
   # Increment num (make sure to do this!)
   num += 1
+
+
+#### Simple Errors
+choice = raw_input('Enjoying the course? (y/n)')
+
+while choice != 'y' and choice != 'n':  # Fill in the condition (before the colon)
+  choice = raw_input("Sorry, I didn't catch that. Enter again: ")
