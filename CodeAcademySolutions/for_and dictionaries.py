16a10682e24889ef957b9a1208c5eb0f2cd21e42
mwongeraE <evansmwongera@gmail.com>
2017-10-12 10:36:47 +0300

    Something of value

--- CodeAcademySolutions/for_and dictionaries.py
+++ CodeAcademySolutions/for_and dictionaries.py
@@ -6,3 +6,28 @@ for food in prices:
   print food
   print "price: %s" % prices[food]
   print "stock: %s" % stock[food]
+
+  #### Something of value
+  prices = {
+  "banana" : 4,
+  "apple"  : 2,
+  "orange" : 1.5,
+  "pear"   : 3,
+}
+stock = {
+  "banana" : 6,
+  "apple"  : 0,
+  "orange" : 32,
+  "pear"   : 15,
+}
+
+for key in prices:
+  print key
+  print "price: %s" % prices[key]
+  print "stock: %s" % stock[key]
+
+
+total = 0
+for item in prices:
+    total += prices[item] * stock[item]
+print total
